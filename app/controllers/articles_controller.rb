2a5e33594e3d3046422620a6813cff0455a10216
Ronald <ronald.scruz16@gmail.com>
2020-04-15 13:29:12 -0300

    Index page done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,12 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
-
   end
 
   private
